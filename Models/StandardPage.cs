08bfc2ab10cff8197d2f0a1c64b98fac15f3dea4
Jenzi <jennydinh94@gmail.com>
2023-03-31 16:21:31 +0200

    Create a standard page

---
+++ Models/StandardPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Piranha.AttributeBuilder;
+using Piranha.Extend;
+using Piranha.Extend.Fields;
+using Piranha.Models;
+
+namespace EG_Piranha.Models
+{
+    [PageType(Title = "Standard Page")]
+    public class SimplePage : Page<SimplePage>
+    {
+    }
+}
+
