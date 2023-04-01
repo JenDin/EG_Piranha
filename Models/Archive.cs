721583fecacb28e8eccab4e0b94fec92ad7d3090
Jenzi <jennydinh94@gmail.com>
2023-04-01 17:30:50 +0200

    Create an archive page

---
+++ Models/Archive.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+using System.IO.Compression;
+
+namespace EG_Piranha.Models
+{
+    [PageType(Title = "Archive Page", UseBlocks = false, IsArchive = true)]
+    public class Archive : Page<Archive>
+    {
+
+    }
+}
