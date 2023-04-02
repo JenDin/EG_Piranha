66eb0a36c597b0304660e15c71a27cd117ad3f4f
Jenzi <jennydinh94@gmail.com>
2023-04-02 14:15:58 +0200

    Create a post model

---
+++ Models/Post.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Piranha.AttributeBuilder;
+using Piranha.Extend;
+using Piranha.Extend.Fields;
+using Piranha.Models;
+
+namespace EG_Piranha.Models
+{
+    [PostType(Title = "Post", UseBlocks = false)]
+    public class Post : PostBase
+	{
+        public class PostReg
+        {
+            [Field]
+            public StringField Title { get; set; }
+
+            [Field(Title = "Top content")]
+            public TextField TopContent { get; set; }
+
+            [Field(Title = "Bottom content (optional)")]
+            public TextField BottomContent { get; set; }
+
+            [Field(Title = "First image")]
+            public ImageField FirstImage { get; set; }
+
+            [Field(Title = "Second image")]
+            public ImageField SecondImage { get; set; }
+
+            [Field(Title = "Third image")]
+            public ImageField ThirdImage { get; set; }
+        }
+
+        [Region]
+        public PostReg PostRegion { get; set; }
+    }
+}
+
