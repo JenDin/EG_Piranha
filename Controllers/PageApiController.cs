d54c4bca995be077b2f0f48e1be744407a1f59ef
Jenzi <jennydinh94@gmail.com>
2023-04-12 14:22:34 +0200

    Refactor controllers

--- Controllers/PageApiController.cs
+++ Controllers/PageApiController.cs
@@ -12,57 +12,25 @@ namespace EG_Piranha.Controllers
     {
         private readonly IApi _api;
 
-        public PageApiController(IApi api, IAuthorizationService auth)
+        public PageApiController(IApi api)
         {
             _api = api;
-        }
-
-        // Gets the page model for the page with the specified id
-        [HttpGet]
-        [Route("{id:Guid}")]
-        public virtual async Task<IActionResult> GetById(Guid id)
-        {
-            return Json(await _api.Pages.GetByIdAsync<PageBase>(id));
         }
 
         // Gets the page model for the page with the specified slug in the default site
         [HttpGet]
         [Route("{slug}")]
-        public virtual async Task<IActionResult> GetBySlug(string slug)
+        public async Task<IActionResult> GetBySlug(string slug)
         {
             return Json(await _api.Pages.GetBySlugAsync<PageBase>(slug));
-        }
-
-        // Gets the page model for the page with the specified site and slug
-        [HttpGet]
-        [Route("{siteId}/{slug}")]
-        public virtual async Task<IActionResult> GetBySlugAndSite(Guid siteId, string slug)
-        {
-            return Json(await _api.Pages.GetBySlugAsync<PageBase>(slug, siteId));
-        }
-
-        // Gets the page info model for the page with the specified id
-        [HttpGet]
-        [Route("info/{id:Guid}")]
-        public virtual async Task<IActionResult> GetInfoById(Guid id)
-        {
-            return Json(await _api.Pages.GetByIdAsync<PageInfo>(id));
         }
 
         // Gets the page info model for the page with the specified slug
         [HttpGet]
         [Route("info/{slug}")]
-        public virtual async Task<IActionResult> GetInfoBySlug(string slug)
+        public async Task<IActionResult> GetInfoBySlug(string slug)
         {
             return Json(await _api.Pages.GetBySlugAsync<PageInfo>(slug));
-        }
-
-        // Gets the page info model for the page with the specified slug and site
-        [HttpGet]
-        [Route("info/{siteId}/{slug}")]
-        public virtual async Task<IActionResult> GetInfoBySlugAndSite(Guid siteId, string slug)
-        {
-            return Json(await _api.Pages.GetBySlugAsync<PageInfo>(slug, siteId));
         }
     }
 
--- Controllers/PostApiController.cs
+++ Controllers/PostApiController.cs
@@ -15,39 +15,31 @@ namespace EG_Piranha.Controllers
     {
         private readonly IApi _api;
 
-        public PostApiController(IApi api, IAuthorizationService auth)
+        public PostApiController(IApi api)
         {
             _api = api;
         }
 
-        // Gets the post model for the post with the specified id
+        // Gets the post model for all posts in the specified archive
         [HttpGet]
-        [Route("{id:Guid}")]
-        public virtual async Task<IActionResult> GetById(Guid id)
+        [Route("all/{archiveId}")]
+        public async Task<IActionResult> GetAllPosts(Guid archiveId)
         {
-            return Json(await _api.Posts.GetByIdAsync<PostBase>(id));
+            return Json(await _api.Posts.GetAllAsync<Post>(archiveId));
         }
 
         // Gets the post model for the post with the specified archive and slug
         [HttpGet]
         [Route("{archiveId}/{slug}")]
-        public virtual async Task<IActionResult> GetBySlugAndArchive(Guid archiveId, string slug)
+        public async Task<IActionResult> GetBySlugAndArchive(Guid archiveId, string slug)
         {
             return Json(await _api.Posts.GetBySlugAsync<PostBase>(archiveId, slug));
-        }
-
-        [HttpGet]
-        [Route("all/{archiveId}")]
-        public virtual async Task<IActionResult> GetAll(Guid archiveId)
-        {
-            var posts = await _api.Posts.GetAllAsync<Post>(archiveId);
-            return Json(posts);
         }
 
         // Gets the post info model for the post with the specified id
         [HttpGet]
         [Route("info/{id:Guid}")]
-        public virtual async Task<IActionResult> GetInfoById(Guid id)
+        public async Task<IActionResult> GetInfoById(Guid id)
         {
             return Json(await _api.Posts.GetByIdAsync<PostInfo>(id));
         }
@@ -55,7 +47,7 @@ namespace EG_Piranha.Controllers
         // Gets the post info model for the post with the specified archive and slug
         [HttpGet]
         [Route("info/{archiveId}/{slug}")]
-        public virtual async Task<IActionResult> GetInfoBySlugAndSite(Guid archiveId, string slug)
+        public async Task<IActionResult> GetInfoBySlugAndSite(Guid archiveId, string slug)
         {
             return Json(await _api.Posts.GetBySlugAsync<PostInfo>(archiveId, slug));
         }
